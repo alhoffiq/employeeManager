3275f951156f2485cd60033f9e6ee3801e11a4ec
alhoff <andrew.hoff14@yahoo.com>
2020-11-04 00:56:08 -0700

    Update role feature functional

--- server.js
+++ server.js
@@ -18,82 +18,82 @@ connection.connect(function (err) {
 });
 
 function start() {
-    inquirer.prompt([
-        {
-            name: "action",
-            type: "list",
-            message: "What would you like to do?",
-            choices: ["View employees", "View roles", "View departments", "Add employee", "Add role", "Add department", "Update employee's role", "Stop"]
-        },
-    ]).then(answers1 => {
-        switch (answers1.action) {
-            case "View employees": // This mess gets all the data about each employee and adds the manager's name to their listing in an overly complicated way
-                connection.query('SELECT manager_id, employee.id, first_name, last_name, title, salary, department_name FROM employee LEFT JOIN role ON role.id = employee.role_id LEFT JOIN department ON department.id = role.department_id', function (err, res) {
-                    if (err) throw err;
-                    connection.query("SELECT manager.first_name, manager.last_name FROM employee LEFT JOIN employee manager ON employee.manager_id = manager.id", function (err2, res2) {
-                        if (err) throw err;
-                        for (let i = 0; i < res.length; i++) {
-                            if (res[i].manager_id != null) {
-                                res[i].manager_name = res2[i].first_name + " " + res2[i].last_name;
-                            }
-                        }
-                        console.table(res);
-                        start();
-                    });
-                });
-                break;
-            case "View roles": // Gets all the role data
-                connection.query('SELECT title, salary, department_name FROM role LEFT JOIN department ON role.department_id = department.id', function (err, res) {
-                    if (err) throw err;
-                    console.table(res);
-                    start();
-                });
-                break;
-            case "View departments": // Gets all the department data
-                connection.query('SELECT department_name FROM department', function (err, res) {
-                    if (err) throw err;
-                    console.table(res);
-                    start();
-                });
-                break;
-            case "Add employee":
-                let roles = [];
-                let employees = [];
-                connection.query("SELECT title FROM role", function (err, res) {
-                    if (err) throw err;
-                    res.forEach(role => {
-                        roles.push(role.title);
-                    });
-                    connection.query("SELECT CONCAT(first_name, ' ', last_name) AS fullName FROM employee", function (err, res) {
-                        if (err) throw err;
-                        res.forEach(employee => {
-                            employees.push(employee.fullName);
+    let roles = [];
+    let employees = [];
+    connection.query("SELECT title FROM role", function (err, res) { // Puts all roles into an array to start
+        if (err) throw err;
+        res.forEach(role => {
+            roles.push(role.title);
+        });
+        connection.query("SELECT CONCAT(first_name, ' ', last_name) AS fullName FROM employee", function (err, res) { // Puts all employees into an array to start
+            if (err) throw err;
+            res.forEach(employee => {
+                employees.push(employee.fullName);
+            });
+            inquirer.prompt([
+                {
+                    name: "action",
+                    type: "list",
+                    message: "What would you like to do?",
+                    choices: ["View employees", "View roles", "View departments", "Add employee", "Add role", "Add department", "Update employee's role", "Stop"]
+                },
+            ]).then(answers1 => {
+                switch (answers1.action) {
+                    case "View employees": // This mess gets all the data about each employee and adds the manager's name to their listing in an overly complicated way
+                        connection.query('SELECT manager_id, employee.id, first_name, last_name, title, salary, department_name FROM employee LEFT JOIN role ON role.id = employee.role_id LEFT JOIN department ON department.id = role.department_id', function (err, res) {
+                            if (err) throw err;
+                            connection.query("SELECT manager.first_name, manager.last_name FROM employee LEFT JOIN employee manager ON employee.manager_id = manager.id", function (err2, res2) {
+                                if (err) throw err;
+                                for (let i = 0; i < res.length; i++) {
+                                    if (res[i].manager_id != null) {
+                                        res[i].manager_name = res2[i].first_name + " " + res2[i].last_name;
+                                    }
+                                }
+                                console.table(res);
+                                start();
+                            });
                         });
+                        break;
+                    case "View roles": // Gets all the role data
+                        connection.query('SELECT title, salary, department_name FROM role LEFT JOIN department ON role.department_id = department.id', function (err, res) {
+                            if (err) throw err;
+                            console.table(res);
+                            start();
+                        });
+                        break;
+                    case "View departments": // Gets all the department data
+                        connection.query('SELECT department_name FROM department', function (err, res) {
+                            if (err) throw err;
+                            console.table(res);
+                            start();
+                        });
+                        break;
+                    case "Add employee":
                         addEmployee(roles, employees);
-                    });
-                });
-                break;
-            case "Add role":
-                let departments = [];
-                connection.query("SELECT department_name FROM department", function (err, res) {
-                    if (err) throw err;
-                    res.forEach(department => {
-                        departments.push(department.department_name);
-                    });
-                    addRole(departments);
-                });
-                break;
-            case "Add department":
-                addDepartment();
-                break;
-            case "Update employee's role":
-
-                break;
-            case "Stop":
-                connection.end();
-                break;
-        }
-    })
+                        break;
+                    case "Add role":
+                        let departments = [];
+                        connection.query("SELECT department_name FROM department", function (err, res) {
+                            if (err) throw err;
+                            res.forEach(department => {
+                                departments.push(department.department_name);
+                            });
+                            addRole(departments);
+                        });
+                        break;
+                    case "Add department":
+                        addDepartment();
+                        break;
+                    case "Update employee's role":
+                        updateRole(roles, employees);
+                        break;
+                    case "Stop":
+                        connection.end();
+                        break;
+                }
+            })
+        });
+    });
 }
 
 function addEmployee(roles, employees) {
@@ -193,3 +193,35 @@ function addDepartment() {
         });
     });
 };
+
+function updateRole(roles, employees) {
+    inquirer.prompt([
+        {
+            name: "employee",
+            type: "list",
+            message: "Which employee's role would you like to update?",
+            choices: employees
+        }, {
+            name: "role",
+            type: "list",
+            message: "Which role would you like to update them to?",
+            choices: roles
+        },
+    ]).then(answers4 => {
+        let roleId;
+        let employeeId;
+        connection.query("SELECT id FROM role WHERE title = ?", [answers4.role], function (err, res) { // Turns role name into it's ID
+            if (err) throw err;
+            roleId = res[0].id;
+            connection.query("SELECT id FROM employee WHERE CONCAT(first_name, ' ', last_name) = ?", [answers4.employee], function (err, res) { // Turns employee name into it's ID
+                if (err) throw err;
+                employeeId = res[0].id;
+                connection.query('UPDATE employee SET role_id = ? WHERE employee.id = ?', [roleId, employeeId], function (err, res) {
+                    if (err) throw err;
+                    console.log("Employee updated!");
+                    start();
+                });
+            });
+        });
+    });
+}
